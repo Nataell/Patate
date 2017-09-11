795a661f988c3a5baddc9b3f45fd3769ca8e5cb5
Trilunaire <tristan.laurent96@gmail.com>
2017-09-11 17:25:18 +0200

    Add bootstrap in gitignore

--- build/temp/production/fr.ESIR.GreenVentory/sass/config.rb
+++ build/temp/production/fr.ESIR.GreenVentory/sass/config.rb
@@ -1,2 +1,2 @@
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\wamp\www\amap/build/.sass-cache'
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/opt/lampp/htdocs/amapp/build/.sass-cache'
